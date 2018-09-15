5a4a61922b0471c5576fddf78d7f09a171790a37
Tom <tomkuecken@gmail.com>
2018-09-15 13:33:46 -0400

    fix to use day instead of hour

--- fn_get_timestamp.sh
+++ fn_get_timestamp.sh
@@ -3,7 +3,7 @@
 
 function get_timestamp() {
 	numChar=5; # number of chars to take off end
-	timeStamp=$(date +%Y%H%d.%N);
+	timeStamp=$(date +%Y%m%d-%N);
 	endChar="$((${#timeStamp}-$numChar))";
 	echo "$(echo $timeStamp | cut -c1-$endChar)";	
 }
